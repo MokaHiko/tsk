d61d6ccdaaceaea24227fad421f1781424e66977
daoismz <christian.solon@breederdao.io>
2024-11-10 18:37:12 +0800

    initial commit

---
+++ include/tsk.h
@@ -0,0 +1,11 @@
+#include "defines.h"
+
+namespace tsk {
+
+/// @brief Transitions the layout of a vulkan image form current to new.
+///
+/// @param[out] texure Pointer to texture that will be populated. 
+/// @returns the amount of bytes read.
+TUSK_API size_t file_read(const char* file_path, char* buffer, size_t buffer_size);
+
+} // namespace tgfx
---
+++ src/tsk.cpp
@@ -0,0 +1,30 @@
+#include "tsk.h"
+
+#include <fstream>
+#include <cassert>
+
+namespace tsk {
+
+[[nodiscard]] size_t file_read(const char* file_path, char* buffer, size_t buffer_size) {
+	std::ifstream file(file_path, std::ios::ate | std::ios::binary);
+
+	if(!file.is_open()) {
+		return 0;
+	}
+
+	size_t file_size = static_cast<size_t>(file.tellg());
+
+	if(buffer == nullptr) {
+		return file_size;
+	}
+
+	assert(buffer_size >= file_size && "Buffer size is too small!");
+
+	file.seekg(0);
+	file.read(buffer, file_size);
+
+	file.close();
+	return file_size;
+}
+
+} // namespace tgfx
